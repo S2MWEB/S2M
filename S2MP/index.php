a16919bf56c702539a84da66a79fe0288b5b7951
S2MWEB <javiersistemas95@gmail.com>
2018-05-23 00:11:49 -0600

    assasa

---
+++ S2MP/index.php
@@ -0,0 +1,23 @@
+
+<?php require_once('dependences/head.php'); ?>
+  <!-- Add your site or application content here -->
+  <!--header-->
+  <header>
+        <p>Binevenido a S2M</p>
+        <nav>
+          <ul>
+            <li>inicio</li>
+            <li>Servicios</li>
+            <li>Clientes</li>
+            <li>Contacto</li>
+          </ul>
+        </nav>
+  </header>
+
+  <!--Body-->
+  --Aqui va el contenido
+  <!--footer-->
+  <footer>
+    @S2M Todos Los derechos reservados
+  </footer>
+<?php require_once('dependences/foot.php'); ?>
