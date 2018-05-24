d995e7cd1ca3fa161353fbccf17e748bdd130be2
S2MWEB <javiersistemas95@gmail.com>
2018-05-23 23:10:53 -0600

    Add files via upload

--- S2MP/index.php
+++ S2MP/index.php
@@ -3,19 +3,59 @@
   <!-- Add your site or application content here -->
   <!--header-->
   <header>
-        <p>Binevenido a S2M</p>
-        <nav>
-          <ul>
-            <li>inicio</li>
-            <li>Servicios</li>
-            <li>Clientes</li>
-            <li>Contacto</li>
-          </ul>
+        <!--<p>Binevenido a S2M</p>-->
+
+        <!--Nav de bootstrap-->
+        <nav class="navbar navbar-expand-lg navbar-fixed-top navbar-dark bg-dark">
+          <!--<a class="navbar-brand" href="#">Navbar</a>-->
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Inicio<span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Servicios</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Clientes
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="#">Action</a>
+                  <a class="dropdown-item" href="#">Another action</a>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Contacto</a>
+              </li>
+              <!--<li class="nav-item">
+                <a class="nav-link disabled" href="#">Disabled</a>
+              </li>-->
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+              <input class="form-control mr-sm-2" type="search" placeholder="Busqueda" aria-label="Search">
+              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Busqueda</button>
+            </form>
+          </div>
         </nav>
+        <div class="slider">
+          <img src="imgs/slider_01.png" alt="" style="width:100%;border:0px;">
+        </div>
+
+
   </header>
 
   <!--Body-->
   --Aqui va el contenido
+  Like I did in the above pictures, you can easily achieve this look with using image editing applications like Photoshop, ImageReady or Fireworks. But lets think of a scenario in which you need to change the look of your website and the previous border color you used around your images doesn't fit with your new design. You will need to open up your images one by one and replace that border. If you have 3 or 4 images it will take minutes, but think of having 50 or more images...
+
+You can achieve the same effect very easily with using pure CSS and it will take you seconds to change the color of the borders in the future.
   <!--footer-->
   <footer>
     @S2M Todos Los derechos reservados
